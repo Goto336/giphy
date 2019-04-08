da066b9c5b33cc4fe0f44e196e17861cc57ac47e
Zeyd Alabsi <gotozeyd336@gmail.com>
2019-04-08 22:26:56 +0000

    first edit git status git status

--- js/script.js
+++ js/script.js
@@ -3,8 +3,18 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+var api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
+
 $("#search-button").click(function(){
-  
+   $.ajax({
+        url: api_url,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+        
+        alert(response.data);
+        }
+    }); 
   
   
 });
