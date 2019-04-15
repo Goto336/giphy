806ef7da08e470a82b3486ba079cba555013fac6
Zeyd Alabsi <gotozeyd336@gmail.com>
2019-04-15 22:14:51 +0000

    third edit

--- js/script.js
+++ js/script.js
@@ -6,16 +6,26 @@
 var api_url = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
 
 $("#search-button").click(function(){
+    
+    var searchTerm = $("#search-term").val();
+    
+    var api_url = "https://api.giphy.com/v1/gifs/search?q="+searchTerm+"&rating=pg&api_key=dc6zaTOxFJmzC"
+
+    
    $.ajax({
         url: api_url,
         method: "GET",
         success: function(response) {
             // YOUR CODE GOES HERE
-        
-        alert(response.data);
-        }
+            $(".container--results").html('');
+            
+            response.data.forEach(function(item){
+               $(".container--results").append(
+            '<img width="150px" src="' + item.images.original.url + '">'
+            );  
+            })
+        }   
     }); 
-  
   
 });
 
